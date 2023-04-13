645e080d72f2f3d77e667a66b3a6dc1f89ad58fb
Dmitry <123workemail@>
2023-04-13 21:50:47 +0300

    added new code to save.py

--- save.py
+++ save.py
@@ -8,7 +8,9 @@ class ExcelLoader:
         self.table = pd.DataFrame()
 
     # open file
-    
+    def openfile(self):
+        self.table = pd.read_excel(self.path, sheet_name=self.sheet_name)
+        print("openfile is done!")
 
     # read file
 
@@ -20,6 +22,11 @@ class ExcelDumper:
         self.table = pd.DataFrame()
 
     # write file
+    def writefile(self, filename, samples: dict):
+        with pd.ExcelWriter(filename) as writer:
+            for sample_name, sample in samples.items():
+                sample.to_excel(writer, sheet_name=sample_name)
+        print("writefile is done!")
 
 
 
